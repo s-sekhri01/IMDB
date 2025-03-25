9290b2860bfce0674bb96b3330ff8b13d59eb7c2
Saransh Sekhri <saranshsekhri@gmail.com>
2025-03-25 23:41:58 +0530

    Watchlist using Props

--- src/Pages/WatchlistPage.jsx
+++ src/Pages/WatchlistPage.jsx
@@ -3,12 +3,14 @@ const WatchListPage = ({ watchlist }) => {
   return (
     <div>
       <h1>Watchlist</h1>
-      <table>
+      <table border={1}>
         <tr>
+          <td>Movie Id</td>
           <td>Movie Name</td>
         </tr>
         {Object.values(watchlist).map((movie) => (
           <tr>
+            <td>{movie.id}</td>
             <td>{movie.title}</td>
           </tr>
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,23 @@
 import { Link } from 'react-router';
-import '../App.css'; 
+import '../App.css';
 
 const Header = () => {
-  return (
-    <header className="header">
-      <h1>IMDB</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Movie List</Link>
-          </li>
-          <li>
-            <Link to="/details">Movie Details</Link>
-          </li>
-          <li>
-            <a href="/watchlist">Watchlist</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+    return (
+        <div className="header">
+            <h1>IMDB</h1>
+            <ul>
+                <li>
+                    <Link to="/"> Movies</Link>
+                </li>
+                <li>
+                    <Link to="/details">Detail</Link>
+                </li>
+                <li>
+                    <Link to="/watchlist">Watch List</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
 
 export default Header;
